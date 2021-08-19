fb5cb44e7a3fcaaa8bbf643f5472efffc8b9ba66
coollpice <coollpice@naver.com>
2021-08-19 21:42:18 +0900

    HTTP 응답 완료

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        return new ModelAndView("response/hello.html").addObject("data", "Hello");
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "Hello!!");
+        return "response/hello.html";
+    }
+
+
+}
